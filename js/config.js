2ede0c714ddb39b7d4aeaed6e83ad155a7309a5d
jui-cesa <juliosdahora@gmail.com>
2026-04-12 19:44:48 -0300

    .

---
+++ js/config.js
@@ -0,0 +1,8 @@
+/* ========================================
+   LIMIAR: CONTROLE DE DANOS
+   ======================================== */
+
+window.LIMIAR_CONFIG = {
+  SUPABASE_URL: 'https://snjkfserlnqnsgtxtkdh.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuamtmc2VybG5xbnNndHh0a2RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwMTQzMjcsImV4cCI6MjA5MTU5MDMyN30.sbLkULzxLuLlBnYSYdVNN32RJBmPPw-BJmHq6YB5WVg'
+};
